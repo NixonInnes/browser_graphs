d25c59d1e17ffd5f7939b83bb6d1edb34f004e1e
James NixonInnes <nixoninnes@gmail.com>
2016-04-04 17:13:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,24 @@ import pandas as pd
 from plotly.offline import plot
 from plotly.graph_objs import Figure, Layout, Scatter
 
-wb = Workbook('Example_Workbook.xlsx')
+wb = Workbook('Phase 1.xlsx')
 graph_title = Range('Dash', 'B2').value
 sheetname = Range('Dash', 'B3').value
+x_axis = Range('Dash', 'B4').value or None
+
 
 def new_df(sheetname, startcell='A1'):
     data = Range(sheetname, startcell).table.value
     temp_df = pd.DataFrame(data[1:], columns=data[0])
     return temp_df
 
-def draw_traces(df):
+def draw_traces(df, x=None):
     traces = []
-    for d in df.columns:
+    if x is not None and x in df.columns:
+        X = df[x]
+    else:
+        X = df.index
+    for d in [d for d in df.columns if d != x]:
         traces.append(
             Scatter(
                 x=X, 
@@ -25,8 +31,7 @@ def draw_traces(df):
     return traces
 
 df = new_df(sheetname)
-X = df.index
-data = draw_traces(df)
+data = draw_traces(df, x=x_axis)
 
 layout = Layout(
     title = graph_title,
@@ -35,5 +40,4 @@ layout = Layout(
 )
 
 figure = Figure(data=data, layout=layout)
-
 plot(figure)
